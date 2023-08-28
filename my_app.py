c7dec6a42568d7680bab0ea0c28820e98b37d4b7
fktrctq11 <142801038+fktrctq11@users.noreply.github.com>
2023-08-28 15:04:04 +0300

    Add files via upload
    v 1.0

---
+++ my_app.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import *
+class MainWin (QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        pass
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+    def show(self):
+        pass
